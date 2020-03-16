5620c7d462df37d9364f890ac894fe4b6460813e
Yannik Wissner <wissner@neoskop.de>
2020-03-16 14:14:58 +0100

    refactor: extract helpers into separate file

--- src/GtmComponents.tsx
+++ src/GtmComponents.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import { GtmComponentProps, CookieRestriction } from './types';
+import { checkCookieRestriction } from './helpers';
 
 export const Script: React.FC<GtmComponentProps> = ({
   id,
   cookieRestriction,
-}) => (
-  <>
-    {loadScript(cookieRestriction) && (
-      <script
-        key="gtm"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+}) =>
+  checkCookieRestriction(cookieRestriction) ? (
+    <script
+      key="gtm"
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
   })(window,document,'script','dataLayer','${id}');`,
-        }}
-      />
-    )}
-  </>
-);
+      }}
+    />
+  ) : null;
 
 export const NoScript: React.FC<GtmComponentProps> = ({ id }) => (
   <noscript>
@@ -31,20 +29,3 @@ export const NoScript: React.FC<GtmComponentProps> = ({ id }) => (
     />
   </noscript>
 );
-
-const loadScript = (restriction: CookieRestriction): boolean => {
-  if (!restriction) {
-    return true;
-  }
-  return (
-    typeof document !== undefined &&
-    getCookieValue(restriction.cookieName) === restriction.value
-  );
-};
-
-function getCookieValue(cookieName: string): string | boolean {
-  const value = document.cookie.match(
-    '(^|;)\\s*' + cookieName + '\\s*=\\s*([^;]+)'
-  );
-  return value ? value.pop() : '';
-}
---
+++ src/helpers.ts
@@ -0,0 +1,21 @@
+import { CookieRestriction } from './types';
+
+export const checkCookieRestriction = (
+  restriction: CookieRestriction
+): boolean => {
+  if (!restriction) {
+    return true;
+  }
+
+  return (
+    typeof document !== undefined &&
+    getCookieValue(restriction.cookieName) === restriction.value
+  );
+};
+
+export const getCookieValue = (cookieName: string): string | boolean => {
+  const value = document.cookie.match(
+    '(^|;)\\s*' + cookieName + '\\s*=\\s*([^;]+)'
+  );
+  return value ? value.pop() : '';
+};
